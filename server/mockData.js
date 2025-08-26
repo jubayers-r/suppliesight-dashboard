bdfda5acd1b065d736560ceb2cb422e0a78e42f8
Jubayer Shikder <validjubayer@gmail.com>
2025-08-26 09:58:20 +0600

    chore(data): create mockData.js for GraphQL server testing

---
+++ server/mockData.js
@@ -0,0 +1,19 @@
+export const products = [
+  { "id": "P-1001", "name": "12mm Hex Bolt", "sku": "HEX-12-100", "warehouse": "BLR-A", "stock": 180, "demand": 120 },
+  { "id": "P-1002", "name": "Steel Washer", "sku": "WSR-08-500", "warehouse": "BLR-A", "stock": 50, "demand": 80 },
+  { "id": "P-1003", "name": "M8 Nut", "sku": "NUT-08-200", "warehouse": "PNQ-C", "stock": 80, "demand": 80 },
+  { "id": "P-1004", "name": "Bearing 608ZZ", "sku": "BRG-608-50", "warehouse": "DEL-B", "stock": 24, "demand": 120 },
+  { "id": "P-1005", "name": "10mm Allen Key", "sku": "ALL-10-75", "warehouse": "BLR-B", "stock": 75, "demand": 50 },
+  { "id": "P-1006", "name": "PVC Pipe 1 inch", "sku": "PVC-25-300", "warehouse": "PNQ-A", "stock": 200, "demand": 150 },
+  { "id": "P-1007", "name": "Rubber Gasket", "sku": "GSK-05-500", "warehouse": "DEL-A", "stock": 300, "demand": 280 },
+  { "id": "P-1008", "name": "Hose Clamp 2 inch", "sku": "CLP-50-100", "warehouse": "BLR-A", "stock": 90, "demand": 60 },
+  { "id": "P-1009", "name": "Brass Coupling", "sku": "CPL-20-30", "warehouse": "PNQ-B", "stock": 30, "demand": 45 },
+  { "id": "P-1010", "name": "Cable Tie 6 inch", "sku": "CBT-06-1000", "warehouse": "DEL-C", "stock": 600, "demand": 700 },
+  { "id": "P-1011", "name": "Thread Seal Tape", "sku": "TST-12-25", "warehouse": "BLR-C", "stock": 100, "demand": 90 },
+  { "id": "P-1012", "name": "Stainless Screw M4", "sku": "SCR-04-250", "warehouse": "PNQ-C", "stock": 250, "demand": 240 },
+  { "id": "P-1013", "name": "Plastic Cap 10mm", "sku": "CAP-10-100", "warehouse": "DEL-B", "stock": 40, "demand": 60 },
+  { "id": "P-1014", "name": "Grease 100g", "sku": "GRS-100-20", "warehouse": "BLR-A", "stock": 20, "demand": 35 },
+  { "id": "P-1015", "name": "Spring Washer M10", "sku": "SWR-10-150", "warehouse": "PNQ-A", "stock": 150, "demand": 100 }
+]
+
+
